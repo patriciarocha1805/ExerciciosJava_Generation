90b4621a3919b4ab2d082ac276904341a9b87ccb
Patrícia Rocha <ptc.patirocha@gmail.com>
2026-04-17 20:36:05 -0300

    Primeiro commit - Atividade prátiva JAVA1

---
+++ src/atividadePraticaJava1/atividade1.java
@@ -0,0 +1,26 @@
+package atividadePraticaJava1;
+
+import java.util.Scanner;
+
+public class atividade1 {
+
+	public static void main(String[] args) {
+		
+		float salarioAtual, abono, novoSalario;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite o seu salário atual: ");
+		salarioAtual = leia.nextFloat();
+		
+		System.out.println("Digite o valor do abono: ");
+		abono = leia.nextFloat();
+		
+		novoSalario = salarioAtual + abono;
+		
+		System.out.println("Seu novo salário é " + novoSalario + " reais.");
+		
+		leia.close();
+
+	}
+
+}
---
+++ src/atividadePraticaJava1/atividade2.java
@@ -0,0 +1,27 @@
+package atividadePraticaJava1;
+
+import java.util.Scanner;
+
+public class atividade2 {
+
+	public static void main(String[] args) {
+		float nota1, nota2, nota3, nota4, media;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Escreva a primeira nota: ");
+		nota1 = leia.nextFloat();
+		System.out.println("Escreva a segunda nota: ");
+		nota2 = leia.nextFloat();
+		System.out.println("Escreva a terceira nota: ");
+		nota3 = leia.nextFloat();
+		System.out.println("Escreva a quarta nota: ");
+		nota4 = leia.nextFloat();
+		
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("A média desse participante é: " + media + ".");
+		leia.close();
+
+	}
+
+}
